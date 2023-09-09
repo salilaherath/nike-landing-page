c13f1620e1f033a6e195e5a7f6236a82b5ab9910
Salila Herath <salilaherath@gmail.com>
2023-09-09 15:53:37 +0530

    Folder structure

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Footer,
+  CustomerReviews,
+  Subscribe,
+  SuperQuality,
+};
